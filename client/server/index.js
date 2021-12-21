3b90340cb1b528be1c2e018f6b1383670a76a5e0
EC2 Default User <ec2-user@ip-172-31-10-176.us-west-1.compute.internal>
2021-12-21 20:36:57 +0000

    Adding SSL cert and moving app to port 80

--- client/server/index.js
+++ client/server/index.js
@@ -1,12 +1,38 @@
 const path = require("path");
 const express = require("express");
-const app = express(); // create express app
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+
+// express app
+const app = express();
+
+//certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/qjmtech.me/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/qjmtech.me/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/qjmtech.me/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
+app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
-// start express server on port 5000
-app.listen(5000, () => {
-    console.log("server started on port 5000");
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
 });
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
