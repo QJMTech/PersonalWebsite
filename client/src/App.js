9606e73c355dd4e2a00ce63d5eb549fae7a2a1b8
Quinn Melssen <qjm7@nau.edu>
2021-12-20 22:46:52 -0700

    Adding express

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import "./App.css";
-import CustomParticles from "./components/landingpage/CustomParticles.js";
-import NameCard from "./components/landingpage/NameCard.js";
+import React from "react";
+import axios from "axios";
+import CustomParticles from "./components/landingpage/CustomParticles"
+import NameCard from "./components/landingpage/NameCard"
 
-function App() {
-    return (
-        <div>
-            <CustomParticles />
-            <NameCard />
-        </div>
-    );
+export default class App extends React.Component {
+    state = {
+        users: [],
+    };
+    componentDidMount() {
+        axios.get("/users.json").then((response) => {
+            this.setState({ users: response.data });
+        });
+    }
+
+    render() {
+        const { users } = this.state;
+        return (
+            <div>
+                <CustomParticles />
+                <NameCard />
+            </div>
+        );
+    }
 }
-
-export default App;
