31156854d7528617c40cb133c03e3cc0133db947
Quinn Melssen <qjm7@nau.edu>
2022-03-08 17:16:18 -0700

    Adding link

--- client/src/components/landingpage/Copyright.js
+++ client/src/components/landingpage/Copyright.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "./Copyright.module.css";
+import { Link } from "react-router-dom"
 
 const Copyright = () => {
-    return (<p className = {styles.copyright}>copyright @2021 Quinn Melssen</p>)
+    return (<p className = {styles.copyright}><Link to = "/momspage"> copyright @2021 Quinn Melssen</Link></p>)
 }
 
 export default Copyright;
